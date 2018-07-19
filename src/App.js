ccf24a958f2376ee5d84e9bb120f728099e3abee
Hoslack Ochieng <hoslackochieng@gmail.com>
2018-07-19 13:03:36 +0300

    [feaure] Create menu commponent
    [Delivers 159165098]

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import SignUp from "./SignUp";
+import Menu from "./Menu";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Home} />
           <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/menu" component={Menu} />
         </div>
       </BrowserRouter>
     );
--- src/Menu.js
+++ src/Menu.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+const Menu = props => {
+  return (
+    <div>
+      <h1>Menu Hello World</h1>
+    </div>
+  );
+};
+
+export default Menu;
